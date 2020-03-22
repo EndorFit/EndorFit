a429490c181b442dac4de57cd058bab4f781a9b9
jacoboDevelops <61711832+jacoboDevelops@users.noreply.github.com>
2020-03-22 20:32:39 +0100

    You can use middle button now

--- EndorFit/app/src/main/java/org/wmii/endorfit/MainWindowActivity.java
+++ EndorFit/app/src/main/java/org/wmii/endorfit/MainWindowActivity.java
@@ -1,6 +1,5 @@
 package org.wmii.endorfit;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,31 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainWindowActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageViewLeft, imageViewCenter, imageViewProfile;
-
-    FirebaseAuth mAuth;
-    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
 
-        mAuth = FirebaseAuth.getInstance();
-        user = mAuth.getCurrentUser();
-        if(user == null) {
-            Intent intent = new Intent(MainWindowActivity.this, MainActivity.class);
-            startActivity(intent);
-        }
-
         findViewById(R.id.imageViewRightIcon).setOnClickListener(this);
         findViewById(R.id.imageViewCenterIcon).setOnClickListener(this);
-
     }
 
     @Override
@@ -40,26 +25,13 @@ public class MainWindowActivity extends AppCompatActivity implements View.OnClic
         switch (view.getId()){
             case R.id.imageViewRightIcon:
                 Intent intent = new Intent(MainWindowActivity.this,ProfileActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             case R.id.imageViewCenterIcon:
                 Intent intentwork = new Intent(MainWindowActivity.this,workoutPlan.class);
                 intentwork.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intentwork);
         }
-
-<<<<<<< HEAD
-=======
-        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                user = firebaseAuth.getCurrentUser();
-                if(user == null) {
-                    Intent intent = new Intent(MainWindowActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
->>>>>>> 08c580a090fc95c252363bdf84f5e95051e73094
 
     }
 }
