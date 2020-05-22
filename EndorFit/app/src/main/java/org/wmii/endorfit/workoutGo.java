cbd233aafb616cd5c3d2e7e57376ed192adfabe7
jacoboDevelops <61711832+jacoboDevelops@users.noreply.github.com>
2020-05-22 19:08:59 +0200

    finaly made this gynamic view work now put this whole data to database and make rest of the activities

--- EndorFit/app/src/main/java/org/wmii/endorfit/workoutGo.java
+++ EndorFit/app/src/main/java/org/wmii/endorfit/workoutGo.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.service.wallpaper.WallpaperService;
+import android.text.DynamicLayout;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -21,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class workoutGo extends AppCompatActivity {
     private GridLayout mleyout;
@@ -34,9 +37,9 @@ public class workoutGo extends AppCompatActivity {
     FirebaseUser users;
     Spinner spinnerType;
     View view;
+    ArrayList<EditText> allExer = new ArrayList<EditText>();
 
-
-    int counter=4;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +53,9 @@ public class workoutGo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-              addView(view);
-                counter+=4;
+                addView(view);
+                counter += 4;
+
             }
 
         });
@@ -66,17 +70,23 @@ public class workoutGo extends AppCompatActivity {
         saveWorkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tab= new Exercise[counter/4];
+                //  tab= new Exercise[counter/4];
+                if (allExer.get(0) == null) {
+                    name.setText("nope");
+                } else {
+                    name.setText(allExer.get(0).getText() );
+
+                }
+
 
                 //todo wydobyć dane z dynamicznego widoku !!!!!!!
                 //todo zaplanować baze przechowujaca treningi cwiczenia etc
                 //todo zapakowac wszystko do struktur exercise Workout
                 //testowy kawalek kodu sztywne dodanie cwiczenia->workoutu ->baza
-                tab[0]= new Exercise("nazwa","Exercise without weights",6,6,6);
-                Workout nowy=new Workout("0",name.toString(),tab);
-                exerciseRef = database.getReference(users.getUid() + "exercises/"+name );
-                exerciseRef.setValue(tab[0]);
-
+                //tab[0]= new Exercise("nazwa","Exercise without weights",6,6,6);
+                // Workout nowy=new Workout("0",name.toString(),tab);
+                // exerciseRef = database.getReference(users.getUid() + "exercises/"+name );
+                // exerciseRef.setValue(tab[0]);
 
 
             }
@@ -84,13 +94,19 @@ public class workoutGo extends AppCompatActivity {
 
 
     }
-    public void addView(View view)
-    {
-        this.view=view;
+
+    public void addView(View view) {
+        this.view = view;
         dnv = new dynamicViews(context);
-        mleyout.addView(dnv.descriptionTextView(getApplicationContext()), counter);
-        mleyout.addView(dnv.repsEditText(getApplicationContext()), counter+1);
-        mleyout.addView(dnv.setsEditText(getApplicationContext()), counter+2);
-        mleyout.addView(dnv.weightEditText(getApplicationContext()), counter+3);
+        allExer.add( dnv.descriptionTextView(getApplicationContext()));
+        allExer.add(dnv.repsEditText(getApplicationContext()));
+        allExer.add( dnv.setsEditText(getApplicationContext()));
+        allExer.add( dnv.weightEditText(getApplicationContext()));
+
+        mleyout.addView(allExer.get(counter), counter);
+        mleyout.addView(allExer.get(counter+1), counter + 1);
+        mleyout.addView(allExer.get(counter+2), counter + 2);
+        mleyout.addView(allExer.get(counter+3), counter + 3);
+
     }
 }
