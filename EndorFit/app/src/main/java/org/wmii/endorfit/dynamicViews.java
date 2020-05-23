3b415a060058fd33437d13fb518d6737bb029b61
jacoboDevelops <61711832+jacoboDevelops@users.noreply.github.com>
2020-05-23 17:03:33 +0200

    Working on database connection for now

--- EndorFit/app/src/main/java/org/wmii/endorfit/dynamicViews.java
+++ EndorFit/app/src/main/java/org/wmii/endorfit/dynamicViews.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.text.InputType;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,6 +22,7 @@ public class dynamicViews {
         editText.setMinEms(2);
         editText.setTextColor(Color.rgb(0,0,0));
         editText.setInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setHint("Name");
         return editText;
     }
     public EditText repsEditText(Context context){
@@ -31,6 +33,7 @@ public class dynamicViews {
         editText.setMinEms(2);
         editText.setTextColor(Color.rgb(0,0,0));
         editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setHint("Reps");
         return editText;
     }
     public EditText setsEditText(Context context){
@@ -41,6 +44,7 @@ public class dynamicViews {
         editText.setMinEms(2);
         editText.setTextColor(Color.rgb(0,0,0));
         editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setHint("Sets");
         return editText;
     }
     public EditText weightEditText(Context context){
@@ -51,6 +55,20 @@ public class dynamicViews {
         editText.setMinEms(2);
         editText.setTextColor(Color.rgb(0,0,0));
         editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setHint("Weight/Time");
         return editText;
+    }
+
+    public Button buttonWorkout (Context context){
+        final ViewGroup.LayoutParams lparams=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        final Button buttonWorkout=new Button(context);
+        int id=0;
+        buttonWorkout.setId(id);
+        buttonWorkout.setMinEms(2);
+        buttonWorkout.setBackgroundColor(Color.rgb(250,150,100));
+        buttonWorkout.setWidth(30);
+        buttonWorkout.setHeight(30);
+
+        return buttonWorkout;
     }
 }
--- EndorFit/app/src/main/java/org/wmii/endorfit/workoutPlan.java
+++ EndorFit/app/src/main/java/org/wmii/endorfit/workoutPlan.java
@@ -1,22 +1,51 @@
 package org.wmii.endorfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class workoutPlan extends AppCompatActivity implements View.OnClickListener {
-
+    dynamicViews dnv;
     private Button button,starter;
+    Context context;
+    DatabaseReference exerciseRef;
+    FirebaseDatabase database;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+    View view;
+    LinearLayout mleyout;
+    ArrayList<PlanItem> planItems;
+    ArrayList<Button> allExer = new ArrayList<Button>();
+    DataSnapshot dataSnapshot;
+    int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_plan);
         starter=(Button) findViewById(R.id.buttonStart);
         button =(Button) findViewById(R.id.button);
+        mleyout = (LinearLayout) findViewById(R.id.leyout);
+        final ArrayList<PlanItem> planItems=new ArrayList<PlanItem>();
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
@@ -24,13 +53,48 @@ public class workoutPlan extends AppCompatActivity implements View.OnClickListen
                 openActivityWorking();
 
             }
-        }       );
+        }      );
+
         starter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityTimer();
             }
         });
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    Intent intent = new Intent(workoutPlan.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        user = mAuth.getCurrentUser();
+
+        database = FirebaseDatabase.getInstance();
+        planItems.clear();
+        final DatabaseReference planContentRef = database.getReference("users/" + user.getUid() + "/plans/" );
+        planContentRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                planItems.add(Objects.requireNonNull(dataSnapshot.getValue(PlanItem.class)));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+for (i=0;i<planItems.size();i++)
+{   addView(view);
+    allExer.get(i).setText(planItems.get(i).getName());
+}
     }
 
     public void openActivityWorking() {
@@ -49,5 +113,12 @@ public class workoutPlan extends AppCompatActivity implements View.OnClickListen
     public void onClick(View v) {
 
     }
-}
+    public void addView(View view) {
+        this.view = view;
+        dnv = new dynamicViews(context);
+        allExer.add(dnv.buttonWorkout(getApplicationContext()));
+        mleyout.addView(allExer.get(i));
 
+
+    }
+}
